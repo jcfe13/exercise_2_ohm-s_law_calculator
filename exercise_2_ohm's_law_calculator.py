88db9a3f0818965398550ab2de4ae19d3a6b1e64
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-25 17:17:34 +0800

    New line

--- exercise_2_ohm's_law_calculator.py
+++ exercise_2_ohm's_law_calculator.py
@@ -2,12 +2,14 @@
 print("Welcome to simple Ohm's Law Calculator:")
 print("1. Voltage(V)\n2. Current(I)\n3. Resistance(R)")
 missing_value = input("\nSelect the number that you want to calculate: ")
+
 # 2. If the user input '1', it will ask to input values to solve voltage (V = I * R)
 if missing_value == "1":
     current = float(input("\nInput your Current: "))
     resistance = float(input("Input your Resistance: "))
     voltage = current * resistance
     print(f"The value of voltage is: {round(voltage, 2)} Volt/s")
+
 # 3. If the user selects '2', proceed to calculate current (I = V / R)
 elif missing_value == "2":
     voltage = float(input("\nInput your Voltage: "))
@@ -17,6 +19,7 @@ elif missing_value == "2":
     else:
         current = voltage / resistance
         print(f"The value of current is: {round(current, 2)} Ampere/s")
+
 # 4. If the user selects '3', proceed to calculate resistance (R = V / I)
 elif missing_value == "3":
     voltage = float(input("\nInput your Voltage: "))
@@ -26,6 +29,7 @@ elif missing_value == "3":
     else:
         resistance = voltage / current
         print(f"The value of resistance is: {round(resistance, 2)} Ohm/s")
+        
 # 5. If the user enters anything other than '1', '2', or '3', display an error
 else:
     print("\n\033[91mInvalid character/number!\033[0m")
