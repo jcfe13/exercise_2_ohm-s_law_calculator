30700d48d67deb31f1ba956419a98c9c040b41a0
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-25 16:59:56 +0800

    Pseudocode

---
+++ exercise_2_ohm's_law_calculator.py
@@ -0,0 +1,5 @@
+# 1. Ask the user what they want to calculate in Ohm's Law
+# 2. If the user input '1', it will ask to input values to solve voltage (V = I * R)
+# 3. If the user selects '2', proceed to calculate current (I = V / R)
+# 4. If the user selects '3', proceed to calculate resistance (R = V / I)
+# 5. If the user enters anything other than '1', '2', or '3', display an error
