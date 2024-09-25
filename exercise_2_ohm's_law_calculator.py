913641d3d9b8f296bdc328b01d18ed9dc290cd4b
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-25 17:03:34 +0800

    If the user selects '1', proceed to calculate voltage

--- exercise_2_ohm's_law_calculator.py
+++ exercise_2_ohm's_law_calculator.py
@@ -3,6 +3,7 @@ print("Welcome to simple Ohm's Law Calculator:")
 print("1. Voltage(V)\n2. Current(I)\n3. Resistance(R)")
 missing_value = input("\nSelect the number that you want to calculate: ")
 # 2. If the user input '1', it will ask to input values to solve voltage (V = I * R)
+if missing_value == "1":
 # 3. If the user selects '2', proceed to calculate current (I = V / R)
 # 4. If the user selects '3', proceed to calculate resistance (R = V / I)
 # 5. If the user enters anything other than '1', '2', or '3', display an error
