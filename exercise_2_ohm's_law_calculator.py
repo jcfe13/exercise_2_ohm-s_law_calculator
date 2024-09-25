d8193a0df00b265145ab4695ad4cd5cae5dace23
jcfe13 <johncarloesquillo72@gmail.com>
2024-09-25 17:04:21 +0800

    Ask the user to input the value for current (I) and resistance (R)

--- exercise_2_ohm's_law_calculator.py
+++ exercise_2_ohm's_law_calculator.py
@@ -4,6 +4,8 @@ print("1. Voltage(V)\n2. Current(I)\n3. Resistance(R)")
 missing_value = input("\nSelect the number that you want to calculate: ")
 # 2. If the user input '1', it will ask to input values to solve voltage (V = I * R)
 if missing_value == "1":
+    current = float(input("\nInput your Current: "))
+    resistance = float(input("Input your Resistance: "))
 # 3. If the user selects '2', proceed to calculate current (I = V / R)
 # 4. If the user selects '3', proceed to calculate resistance (R = V / I)
 # 5. If the user enters anything other than '1', '2', or '3', display an error
